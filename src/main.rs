a94342fcc17974af0ffb7fabcaa0993f45bab0a5
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2022-02-06 18:15:20 -0600

    working on crate rgb2ansi256

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use rgb2ansi256::rgb_to_ansi256;
+
 fn main() {
     println!("Hello, world!");
+    let colors = vec!["255;74;68", "122;112;112", "102;217;239"];
+    for c in colors {
+        let sp: Vec<&str> = c.split(";").collect();
+        let r = sp[0].parse::<u8>().unwrap();
+        let g = sp[1].parse::<u8>().unwrap();
+        let b = sp[2].parse::<u8>().unwrap();
+        let co = rgb_to_ansi256(r, g, b);
+        println!("c: {} co: {}", c, co);
+    }
 }
